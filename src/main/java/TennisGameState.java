0d27fe3689f1e84ae007a8677da7e7dfe05c9a39
Xavier NOPRE <xnopre@gmail.com>
2018-05-15 13:16:05 +0200

    Etape : DEUCE avec if

--- src/main/java/TennisGameState.java
+++ src/main/java/TennisGameState.java
@@ -1,7 +1,7 @@
 public class TennisGameState {
 
-    private int pointsPlayer1;
-    private int pointsPlayer2;
+    private final int pointsPlayer1;
+    private final int pointsPlayer2;
 
     TennisGameState() {
         this(0,0);
@@ -13,22 +13,27 @@ public class TennisGameState {
     }
 
     public boolean notFinished() {
-        return true;
+        return pointsPlayer1 != 4 && pointsPlayer2 != 4;
     }
 
     public String winner() {
-        return null;
+        return pointsPlayer1 == 4 ? "Player 1" : "Player 2";
     }
 
     public String currentScore() {
+        if (pointsPlayer1 >= 3 && pointsPlayer2 >=  3) {
+            return "DEUCE";
+        }
         return getScorePlayer(pointsPlayer1) + " - " + getScorePlayer(pointsPlayer2);
     }
 
     private String getScorePlayer(int pointsPlayer) {
-        return pointsPlayer == 0 ? "LOVE" : "FIFTEEN";
+        return SCORE_FROM_POINTS[pointsPlayer];
     }
 
     public TennisGameState computeNextScore(String pointWinner) {
         return new TennisGameState(this.pointsPlayer1+(pointWinner.equals("1") ? 1 : 0), this.pointsPlayer2+(pointWinner.equals("2") ? 1 : 0));
     }
+
+    private static final String[] SCORE_FROM_POINTS = { "LOVE", "FIFTEEN", "THIRTY", "FORTY" };
 }
--- src/test/java/TennisGameTest.java
+++ src/test/java/TennisGameTest.java
@@ -4,23 +4,87 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisGameTest {
 
+    private TennisGameState state = new TennisGameState();
+
     @Test
     public void initial_score_should_be_love_love() {
-        TennisGameState state = new TennisGameState();
+        assertThat(state.notFinished()).isTrue();
         assertThat(state.currentScore()).isEqualTo("LOVE - LOVE");
     }
 
     @Test
     public void score_should_be_fifteen_love_when_first_player_won_first_point() {
-        TennisGameState state = new TennisGameState();
-        TennisGameState nextState = state.computeNextScore("1");
+        TennisGameState nextState = state
+                .computeNextScore("1");
+        assertThat(nextState.notFinished()).isTrue();
         assertThat(nextState.currentScore()).isEqualTo("FIFTEEN - LOVE");
     }
 
     @Test
     public void score_should_be_fifteen_fifteen_when_both_players_won_one_point() {
-        TennisGameState state = new TennisGameState();
-        TennisGameState nextState = state.computeNextScore("1").computeNextScore("2");
+        TennisGameState nextState = state
+                .computeNextScore("1")
+                .computeNextScore("2");
+        assertThat(nextState.notFinished()).isTrue();
         assertThat(nextState.currentScore()).isEqualTo("FIFTEEN - FIFTEEN");
+    }
+
+    @Test
+    public void score_should_be_thirty_fifteen() {
+        TennisGameState nextState = state
+                .computeNextScore("1")
+                .computeNextScore("1")
+                .computeNextScore("2");
+        assertThat(nextState.notFinished()).isTrue();
+        assertThat(nextState.currentScore()).isEqualTo("THIRTY - FIFTEEN");
+    }
+
+    @Test
+    public void score_should_be_thirty_forty() {
+        TennisGameState nextState = state
+                .computeNextScore("1")
+                .computeNextScore("1")
+                .computeNextScore("2")
+                .computeNextScore("2")
+                .computeNextScore("2");
+        assertThat(nextState.notFinished()).isTrue();
+        assertThat(nextState.currentScore()).isEqualTo("THIRTY - FORTY");
+    }
+
+    @Test
+    public void score_should_be_deuce_case_1() {
+        TennisGameState nextState = state
+                .computeNextScore("1")
+                .computeNextScore("1")
+                .computeNextScore("1")
+                .computeNextScore("2")
+                .computeNextScore("2")
+                .computeNextScore("2");
+        assertThat(nextState.notFinished()).isTrue();
+        assertThat(nextState.currentScore()).isEqualTo("DEUCE");
+    }
+
+    // Finished
+
+    @Test
+    public void game_should_be_finished_for_player_1() {
+        TennisGameState nextState = state
+                .computeNextScore("1")
+                .computeNextScore("1")
+                .computeNextScore("1")
+                .computeNextScore("1");
+        assertThat(nextState.notFinished()).isFalse();
+        assertThat(nextState.winner()).isEqualTo("Player 1");
+    }
+
+    @Test
+    public void game_should_be_finished_for_player_2() {
+        TennisGameState nextState = state
+                .computeNextScore("2")
+                .computeNextScore("2")
+                .computeNextScore("2")
+                .computeNextScore("2");
+        assertThat(nextState.notFinished()).isFalse();
+        assertThat(nextState.winner()).isEqualTo("Player 2");
     }
 }
